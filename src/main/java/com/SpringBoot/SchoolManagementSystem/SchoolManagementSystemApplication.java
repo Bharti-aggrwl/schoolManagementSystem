5b7988143905f59db3e3a7e664095e5cf0bdffd6
shivanisrivastava26 <shivani.sri140@gmail.com>
2024-02-17 21:18:48 +0530

    databaseChanges

--- src/main/java/com/SpringBoot/SchoolManagementSystem/SchoolManagementSystemApplication.java
+++ src/main/java/com/SpringBoot/SchoolManagementSystem/SchoolManagementSystemApplication.java
@@ -1,11 +1,13 @@
 package com.SpringBoot.SchoolManagementSystem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 @EntityScan(basePackages = {"com.SpringBoot.SchoolManagementSystem.Entity"})
 public class SchoolManagementSystemApplication {
 
