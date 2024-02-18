391abbed9f0c4f3031ceea258c0f76c2e1c8327b
shivanisrivastava26 <shivani.sri140@gmail.com>
2024-02-18 19:54:20 +0530

    newLoginchange

--- src/main/java/com/SpringBoot/SchoolManagementSystem/Service/LoginService.java
+++ src/main/java/com/SpringBoot/SchoolManagementSystem/Service/LoginService.java
@@ -1,37 +1,38 @@
-//package com.SpringBoot.SchoolManagementSystem.Service;
-//
-//import java.util.Optional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.SpringBoot.SchoolManagementSystem.Configuration.Reply;
-//import com.SpringBoot.SchoolManagementSystem.Configuration.ServiceReplyModel;
-//import com.SpringBoot.SchoolManagementSystem.Entity.LoginTbl;
-//import com.SpringBoot.SchoolManagementSystem.Repository.LoginRepo;
-//
-//@Service
-//public class LoginService {
-// private static LoginRepo loginRepo;
-// 
-//@Autowired
-//	public LoginService(LoginRepo loginRepo) {
-//	this.loginRepo = loginRepo;
-//	
-//}
-//
-//
-//	public ServiceReplyModel getValidateLoginData(String email, String password) {
-//		// TODO Auto-generated method stub
-//		ServiceReplyModel reply = new ServiceReplyModel();
-//		Optional<LoginTbl> loginOptional= loginRepo.findByLoginTblEmailIdAndPassword(email,password);
-//		if(loginOptional.isPresent()) {
-//			 reply.setMessage("login successfully");
-//			 return reply;
-//		}else {
-//			 reply.setMessage("Invalid Email Id");
-//			return reply;
-//		}
-//	}
-//
-//}
+package com.SpringBoot.SchoolManagementSystem.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.SpringBoot.SchoolManagementSystem.Configuration.Reply;
+import com.SpringBoot.SchoolManagementSystem.Configuration.ServiceReplyModel;
+import com.SpringBoot.SchoolManagementSystem.Entity.LoginTbl;
+import com.SpringBoot.SchoolManagementSystem.Repository.LoginRepository;
+
+@Service
+public class LoginService {
+ private static LoginRepository loginRepo;
+ 
+@Autowired
+	public LoginService(LoginRepository loginRepo) {
+	this.loginRepo = loginRepo;
+	
+}
+
+
+	public ServiceReplyModel getValidateLoginData(String email, String password) {
+		// TODO Auto-generated method stub
+		ServiceReplyModel reply = new ServiceReplyModel();
+		List<LoginTbl> loginOptional= loginRepo.findAll();
+		if(loginOptional!=null) {
+			 reply.setMessage("login successfully");
+			 return reply;
+		}else {
+			 reply.setMessage("Invalid Email Id");
+			return reply;
+		}
+	}
+
+}
